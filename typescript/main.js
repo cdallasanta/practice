adbdad06b44c5e4126a74c102d844d6e09b664fe
cdallasanta <chris.dallasanta@gmail.com>
2020-03-24 12:48:37 -0700

    Source updated at 2020-03-24 12:48:36 -0700.

--- typescript/main.js
+++ typescript/main.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = 'Welcome Back';
 console.log(message);
@@ -12,4 +25,40 @@ function addFiveOrN(num1, num2) {
     if (num2 === void 0) { num2 = 5; }
     return num1 + num2;
 }
-console.log(addFiveOrN(10));
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: "Chris",
+    lastName: "Dalla Santa",
+    contactInfo: {
+        phone: 1234,
+        address: "yes",
+        email: "email"
+    }
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning, " + this.employeeName);
+    };
+    return Employee;
+}());
+var bob = new Employee("Bob");
+bob.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegate = function () {
+        console.log(this.employeeName + " delegating tasks");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager("Steven");
+m1.greet();
+m1.delegate();
--- typescript/main.ts
+++ typescript/main.ts
@@ -17,4 +17,60 @@ function addFiveOrN(num1: number, num2:number = 5): number {
   return num1 + num2;
 }
 
-console.log(addFiveOrN(10));
+function fullName(person: Person) {
+  console.log(`${person.firstName} ${person.lastName}`)
+}
+
+interface Person {
+  firstName: string,
+  lastName: string,
+  contactInfo?: ContactInfo
+}
+
+interface ContactInfo {
+  phone: number,
+  address: string,
+  email: string
+}
+
+let p: Person = {
+  firstName: "Chris",
+  lastName: "Dalla Santa",
+  contactInfo: {
+    phone: 1234,
+    address: "yes",
+    email: "email"
+  }
+}
+
+fullName(p);
+
+
+class Employee {
+  protected employeeName: string;
+
+  constructor(name:string){
+    this.employeeName = name;
+  }
+
+  greet(){
+    console.log(`Good morning, ${this.employeeName}`)
+  }
+}
+
+let bob: Employee = new Employee("Bob");
+bob.greet();
+
+class Manager extends Employee {
+  constructor(managerName:string){
+    super(managerName);
+  }
+
+  delegate(){
+    console.log(`${this.employeeName} delegating tasks`);
+  }
+}
+
+let m1: Manager = new Manager("Steven");
+m1.greet();
+m1.delegate();
