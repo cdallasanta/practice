a768e1b777ed0a73584a443bd25431ea5ed97941
cdallasanta <chris.dallasanta@gmail.com>
2020-03-24 12:31:43 -0700

    Source updated at 2020-03-24 12:31:43 -0700.

--- typescript/main.js
+++ typescript/main.js
@@ -5,3 +5,11 @@ console.log(message);
 var name = "Chris";
 var sentence = "My name is " + name + ".\nI am a beginner";
 console.log(sentence);
+function add(num1, num2) {
+    return num1 + num2;
+}
+function addFiveOrN(num1, num2) {
+    if (num2 === void 0) { num2 = 5; }
+    return num1 + num2;
+}
+console.log(addFiveOrN(10));
--- typescript/main.ts
+++ typescript/main.ts
@@ -8,3 +8,13 @@ let sentence: string = `My name is ${name}.
 I am a beginner`;
 console.log(sentence);
 
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function addFiveOrN(num1: number, num2:number = 5): number {
+  return num1 + num2;
+}
+
+console.log(addFiveOrN(10));
