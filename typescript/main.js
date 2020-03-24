93204efc414843953e48c77f9ad7c46629d26812
cdallasanta <chris.dallasanta@gmail.com>
2020-03-24 12:19:42 -0700

    Source updated at 2020-03-24 12:19:42 -0700.

---
+++ typescript/main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome Back';
+console.log(message);
+var name = "Chris";
+var sentence = "My name is " + name + ".\nI am a beginner";
+console.log(sentence);
---
+++ typescript/main.ts
@@ -0,0 +1,10 @@
+export {}
+
+let message: string = 'Welcome Back';
+console.log(message);
+
+let name: string = "Chris"
+let sentence: string = `My name is ${name}.
+I am a beginner`;
+console.log(sentence);
+
